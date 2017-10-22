995d34e2f39c25195bea4eddaaa0a522a024300c
unknown <>
2017-10-22 23:26:14 +0800

    增加del 模块  默认gulp 启动，gulp dist 生产
    Signed-off-by: unknown <董建强>

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var rm = require('del');
 var sass = require('gulp-sass');
 var imageMin = require('gulp-imagemin');
 var concat = require('gulp-concat');
@@ -10,6 +11,12 @@ var browserSync = require('browser-sync');/*本地服务器并且多浏览器自
   gulp.dest - Points to folder to output
   gulp.watch - Watch files and folders for changes
 */
+
+/*清除产出目录*/
+gulp.task('clear-dist', function() {
+    rm.sync(['./dist/**']);
+})
+
 /*本地服务器任务*/
 gulp.task('browserSync', function() {
   browserSync({
@@ -25,55 +32,36 @@ gulp.task('message',function(){
 });
 
 /*文件复制  复制移动*/
-gulp.task('cloneHtml',function(){
-	gulp.src(['src/**/*.html'])
-		.pipe(gulp.dest('dist'))
+gulp.task('cloneFile',function(){
+	gulp.src(['./src/{css,js,lib,fonts,page}/*','./src/*.html'])
+		.pipe(gulp.dest('./dist'))
 		
 });
-gulp.task('cloneLib',function(){
-	gulp.src(['src/lib/*'])
-		.pipe(gulp.dest('dist/lib'))
-		
-});
-gulp.task('cloneFonts',function(){
-	gulp.src(['src/fonts/*'])
-		.pipe(gulp.dest('dist/fonts'))
-		
-});
-gulp.task('cloneJs',function(){
-	gulp.src(['src/js/**/*'])
-		.pipe(gulp.dest('dist/js'))
-		
-});
-gulp.task('cloneCss',function(){
-	gulp.src(['src/css/**/*'])
-		.pipe(gulp.dest('dist/css'))
-		
-});
+
 /*转换sass*/
 gulp.task('sass',function(){
-	gulp.src('src/sass/*.+(scss|sass|css)')
+	gulp.src('./src/sass/*.+(scss|sass|css)')
 		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest('src/css'))
+		.pipe(gulp.dest('./src/css'))
 		.pipe(browserSync.reload({
 	      stream: true
 	    }))
 });
 /*压缩imgage*/
 gulp.task('imageMin',function(){
-    gulp.src('src/img/**/*')
+    gulp.src('./src/img/**/*')
         .pipe(imageMin())
-        .pipe(gulp.dest('dist/img'))
+        .pipe(gulp.dest('./dist/img'))
         
 });
 
 /*默认任务*/
-gulp.task('default',['message','cloneHtml','cloneLib','cloneFonts','cloneJs','cloneCss','imageMin']);
+gulp.task('dist',['message','clear-dist','cloneFile','imageMin']);
 
-gulp.task('watch',['browserSync','sass','imageMin'] ,function(){
-	  gulp.watch('src/sass/**/*.scss', ['sass']);
-	  gulp.watch('src/**/*.html', browserSync.reload);
-	  gulp.watch('src/js/**/*', browserSync.reload);
-	  gulp.watch('src/img/**/*', browserSync.reload);
+gulp.task('default',['browserSync','sass'] ,function(){
+	  gulp.watch('./src/sass/**/*.scss', ['sass']);
+	  gulp.watch('./src/**/*.html', browserSync.reload);
+	  gulp.watch('./src/js/**/*', browserSync.reload);
+	  gulp.watch('./src/img/**/*', browserSync.reload);
 	  // Other watchers
 })
