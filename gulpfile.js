30a2f4d7f3dfb83bd128cc3abd4b591fa2a98880
董先生 <18513552894@163.com>
2018-01-09 09:39:11 +0800

    2.2适配

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var rm = require('del');
 var sass = require('gulp-sass');
 var imageMin = require('gulp-imagemin');
 var concat = require('gulp-concat');
-var browserSync = require('browser-sync').create();/*本地服务器并且多浏览器自动刷新*/
+var browserSync = require('browser-sync').create(); /*本地服务器并且多浏览器自动刷新*/
 var reload = browserSync.reload;
 /*
   -- 常用的方法 --
@@ -14,54 +14,55 @@ var reload = browserSync.reload;
 */
 /*清除产出目录*/
 gulp.task('clear-dist', function() {
-    rm.sync(['./dist/**']);
+	rm.sync(['./dist/**']);
 })
 
 /*message  gulp打印运行提示*/
 
-gulp.task('message',function(){
+gulp.task('message', function() {
 	console.log("gulp is runing")
 });
 
-
 /*文件复制  复制移动*/
-gulp.task('cloneFile',function(){
-	gulp.src(['./src/{css,js,lib,fonts,page}/*','./src/*.html'])
+gulp.task('cloneFile', function() {
+	gulp.src(['./src/{css,js,lib,fonts,page}/*', './src/*.html'])
 		.pipe(gulp.dest('./dist'))
-		
+
 });
 
 /*压缩imgage*/
-gulp.task('imageMin',function(){
-    gulp.src('./src/img/**/*')
-        .pipe(imageMin())
-        .pipe(gulp.dest('./dist/img'))
-        
+gulp.task('imageMin', function() {
+	gulp.src('./src/img/**/*')
+		.pipe(imageMin())
+		.pipe(gulp.dest('./dist/img'))
+
 });
 
-gulp.task('dist',['message','clear-dist','cloneFile','imageMin']);
+gulp.task('dist', ['message', 'clear-dist', 'cloneFile', 'imageMin']);
 
 // scss编译后的css将注入到浏览器里实现更新
 gulp.task('sass', function() {
-    return gulp.src('./src/sass/**/*')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./src/css'))
-        .pipe(reload({stream: true}));
+	return gulp.src('./src/sass/**/*')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest('./src/css'))
+		.pipe(reload({
+			stream: true
+		}));
 });
 
 /*默认任务*/
-gulp.task('default',['sass'] ,function(){
-	
-		browserSync.init({
-        server:{
-        	baseDir: './src',
-        	index:''
-        }
-    });
-	
-	  gulp.watch('./src/sass/**/*', ['sass']);
-	  gulp.watch('./src/**/*.html').on("change",reload);
-	  gulp.watch('./src/js/**/*').on("change",reload);
-	  gulp.watch('./src/img/**/*').on("change",reload);
-	  // Other watchers
+gulp.task('default', ['sass'], function() {
+
+	browserSync.init({
+		server: {
+			baseDir: './src',
+			index: ''
+		}
+	});
+
+	gulp.watch('./src/sass/**/*', ['sass']);
+	gulp.watch('./src/**/*.html').on("change", reload);
+	gulp.watch('./src/js/**/*').on("change", reload);
+	gulp.watch('./src/img/**/*').on("change", reload);
+	// Other watchers
 });
